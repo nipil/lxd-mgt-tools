81f2b5b773dbc44f97f0dbe7c913eedc57f9a2b7
nipil <nipil@users.noreply.github.com>
2017-09-30 13:17:06 +0200

    add usage

--- dmz-create.sh
+++ dmz-create.sh
@@ -2,6 +2,8 @@
 
 [ ${UID} -eq 0 ] || { 1>&2 echo "This script must be run as root" ; exit 1 ; }
 
+[ ${#} -eq 1 ] || { 1>&2 echo "Usage: ${0} DMZ_NUM" ; exit 1 ; }
+
 [ ${1} -ge 1 -a ${1} -le 255 ] || { 1>&2 echo "Invalid DMZ number ${1}" ; exit 1 ; }
 
 DMZ_DEC=$(printf "%d" "${1}")
