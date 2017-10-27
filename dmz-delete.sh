6ca017435304a2e56a8818b038d983d46f5b87c5
nipil <nipil@users.noreply.github.com>
2017-10-27 09:37:12 +0200

    allow 0 as dmz number for delete too

--- dmz-delete.sh
+++ dmz-delete.sh
@@ -4,7 +4,7 @@
 
 [ ${#} -eq 1 ] || { 1>&2 echo "Usage: ${0} DMZ_NUM" ; exit 1 ; }
 
-[ ${1} -ge 1 -a ${1} -le 255 ] || { 1>&2 echo "Invalid DMZ number ${1}" ; exit 1 ; }
+[ ${1} -ge 0 -a ${1} -le 255 ] || { 1>&2 echo "Invalid DMZ number ${1}" ; exit 1 ; }
 
 DMZ_DEC=$(printf "%d" "${1}")
 DMZ_HEX=$(printf "%02x" "${1}")
